1259fffdcc4c518056db46c6c42ab8e3c5e11947
marlybone <41431476+marlybone@users.noreply.github.com>
2023-09-21 19:45:46 +0200

    stopwatch react certification FCC

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, {useState, useEffect} from 'react';
 
 function App() {
+  const [breakLength, setBreakLength] = useState(5);
+  const [sessionLength, setSessionLength] = useState(25);
+
+
+  const handleIncrement = (e) => {
+    switch (e) {
+      case 1:
+        setBreakLength(breakLength + 1)
+        break;
+        case 2:
+        setSessionLength(sessionLength + 1)
+        break;
+        default:
+        break;
+    }
+  }
+
+  const handleDecrement = (e) => {
+    switch (e) {
+      case 1:
+        setBreakLength(breakLength - 1)
+        break;
+        case 2:
+        setSessionLength(sessionLength - 1)
+        break;
+        default:
+        break;
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ <div >
+  <div>
+  <div id='break-label'>Break Length</div>
+  <button id="break-decrement" onClick={(() => handleDecrement(1))}></button>
+  <button id="break-increment" onClick={(() => handleIncrement(1))}></button>
+  <div id="break-length">{breakLength}</div>
+  </div>
+  <div>
+  <div id='session-label'>Session Length</div>
+  <button id="session-decrement" onClick={(() => handleDecrement(2))}></button>
+  <button id="session-increment" onClick={(() => handleIncrement(2))}></button>
+  <div id="session-length">{sessionLength}</div>
+  </div>
+  <div>
+    <div id="timer-label">Session</div>
+    <div id="time-left">{}</div>
+    <div>
+    <button id="start_stop"></button>
+    <button id="reset"></button>
     </div>
+  </div>
+ </div>
   );
 }
 
