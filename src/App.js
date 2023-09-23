d38274d174bbcf5787476c1f57125d57abb382de
marlybone <41431476+marlybone@users.noreply.github.com>
2023-09-24 00:14:19 +0200

    more functionality

--- src/App.js
+++ src/App.js
@@ -7,40 +7,68 @@ function App() {
   const [sessionLength, setSessionLength] = useState(25);
   const [time, setTime] = useState(sessionLength * 60);
   const [timerRunning, setTimerRunning] = useState(false);
+  const [isSession, setIsSession] = useState(true)
 
 
   const handleIncrement = (e) => {
-    if (e === 60) {
-      return;
-    } else {
     switch (e) {
       case 1:
-        setBreakLength(breakLength + 1)
+        if(timerRunning === true) {
+          return;
+        }
+        if (breakLength === 60) {
+          return;
+        } else if (breakLength < 60) {
+        setBreakLength(breakLength + 1);
+        if (!isSession){
+          setTime((breakLength + 1) * 60)
+        }
         break;
+        }
         case 2:
-        setSessionLength(sessionLength + 1)
+          if(timerRunning === true) {
+            return;
+          }
+          if(sessionLength === 60) {
+            return;
+          } else if (sessionLength < 60) {
+        setSessionLength(sessionLength + 1);
+        if (isSession){
+        setTime((sessionLength + 1) * 60)
+        }
         break;
+          }
         default:
         break;
-    }
   }
   }
 
   const handleDecrement = (e) => {
     switch (e) {
       case 1:
-        if (breakLength === 0 || breakLength === 60){
+        if(timerRunning === true) {
           return;
-        } else {  
-        setBreakLength(breakLength - 1)
+        }
+        if (breakLength === 1){
+          return;
+        } else if (breakLength > 1) {  
+        setBreakLength(breakLength - 1);
+        if(!isSession){
+          setTime((breakLength - 1) * 60)
+        }
         break;
         }
         case 2:
-          if (sessionLength === 0 || sessionLength === 60) {
+          if(timerRunning === true) {
+            return;
+          }
+          if (sessionLength === 1) {
           return;
-          } else {
-        setSessionLength(sessionLength - 1)
+          } else if (sessionLength > 1) {
+        setSessionLength(sessionLength - 1);
+        if(isSession) {
         setTime((sessionLength - 1) * 60)
+        }
         break;
           }
         default:
@@ -50,6 +78,17 @@ function App() {
   
 
   useEffect(() => {
+    const beepSound = document.getElementById("beep");
+
+if (timerRunning && time === 0) {
+  beepSound.play();
+
+  setTimeout(() => {
+    beepSound.pause();
+    beepSound.currentTime = 0;
+  }, 2000);
+}
+  
     if (timerRunning && time > 0) {
       const timer = setInterval(() => {
         setTime((prevTime) => prevTime - 1);
@@ -58,8 +97,16 @@ function App() {
       return () => {
         clearInterval(timer);
       };
+    } if (time === 0 && isSession === true){
+      setIsSession(!isSession);
+      setTime(breakLength * 60);
+      setTimerRunning(true)
+    } else if (time === 0 && isSession === false) {
+      setIsSession(true)
+      setTime(sessionLength * 60)
+      setTimerRunning(true)
     }
-  }, [time, timerRunning]);
+  }, [time, timerRunning, isSession, sessionLength, breakLength]);
 
   const formatTime = (seconds) => {
     const minutes = Math.floor(seconds / 60);
@@ -74,26 +121,36 @@ function App() {
   }
 
 
+  const handleReset = () => {
+    setBreakLength(5);
+    setSessionLength(25)
+    setTime(1500)
+    setIsSession(true)
+    
+  }
+
+
   return (
  <div >
   <div>
   <div id='break-label'>Break Length</div>
-  <button id="break-decrement" onClick={(() => handleDecrement(1))}></button>
-  <button id="break-increment" onClick={(() => handleIncrement(1))}></button>
+  <button id="break-decrement" onClick={(() => handleDecrement(1))} >Decrement</button>
+  <button id="break-increment" onClick={(() => handleIncrement(1))} >Increment</button>
   <div id="break-length">{breakLength}</div>
   </div>
   <div>
   <div id='session-label'>Session Length</div>
-  <button id="session-decrement" onClick={(() => handleDecrement(2))}></button>
-  <button id="session-increment" onClick={(() => handleIncrement(2))}></button>
+  <button id="session-decrement" onClick={(() => handleDecrement(2))} >Decrement</button>
+  <button id="session-increment" onClick={(() => handleIncrement(2))} >Increment</button>
   <div id="session-length">{sessionLength}</div>
   </div>
   <div>
-    <div id="timer-label">Session</div>
+    <div id="timer-label">{isSession ? 'Session' : 'Break'}</div>
     <div id="time-left">{formatTime(time)}</div>
     <div>
-    <button id="start_stop" onClick={toggleTimer}> {timerRunning ? 'Pause' : 'Start'}</button>
-    <button id="reset"></button>
+    <button id="start_stop" onClick={toggleTimer}>{timerRunning ? 'Pause' : 'Start'}</button>
+    <button id="reset" onClick={() => handleReset()}>Reset</button>
+    <audio id='beep' src='https://media.geeksforgeeks.org/wp-content/uploads/20190531135120/beep.mp3'></audio>
     </div>
   </div>
  </div>
